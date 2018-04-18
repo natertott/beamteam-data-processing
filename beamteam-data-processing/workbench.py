12182b1597c8e40490f98bfb486fac85715bf9e2
Chris Borg <cborg@citrine.io>
2018-04-18 14:20:55 -0600

    added refinement parsers to workbench. refactored import statements

--- beamteam-data-processing/workbench.py
+++ beamteam-data-processing/workbench.py
@@ -1,6 +1,6 @@
 import argparse
-import xrd_data_parser
-
+from xrd_data_parser import parse_csv, parse_xrdml
+from refinement_parser import parse_refinement
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('csv', nargs='*', help='path to input files')
@@ -9,12 +9,16 @@ if __name__ == '__main__':
 
     for f in args.csv:
 
+        print("PARSING: ", f)
+
         extension = f.split(".")[-1]
 
         if extension == "csv":
-            print(f)
-            system = xrd_data_parser.parse_csv(f)
+            system = parse_csv(f)
 
         elif extension == "xrdml":
+            system = parse_xrdml(f)
+
+        elif extension == "lst":
             print(f)
-            system = xrd_data_parser.parse_xrdml(f)
+            system = parse_refinement(f)
